1669a5755b9d989ba2ff7acf1e77965558d5b8bf
labcap <banlazthosow@gamil.com>
2022-10-07 17:56:15 +0300

    added styles and adaptive to '_header.htm'

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,2 +1,16 @@
 // Подключение функционала "Чертогов Фрилансера"
 // Подключение списка активных модулей
+
+const burger = document.querySelector(".icon-menu");
+const menu = document.querySelector(".nav");
+const searchBtn = document.querySelector(".header__social-item-search");
+const searchForm = document.querySelector(".search-form");
+
+searchBtn.addEventListener("click", () => {
+	searchForm.classList.toggle("open");
+});
+burger.addEventListener("click", () => {
+	burger.classList.toggle("menu-open");
+	menu.classList.toggle("menu-open");
+	document.body.classList.toggle("_lock");
+});
